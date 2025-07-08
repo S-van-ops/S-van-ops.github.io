ea07296adbc011467de2daf392a9fbf8af4fd2c3
S-van-ops <vankayala.tej@gmail.com>
2025-07-08 14:46:08 -0400

    Update jsDemo.js

--- jsDemo.js
+++ jsDemo.js
@@ -6,5 +6,5 @@ console.log(`Name: ${name}`);
 console.log(`School: ${school}`);
 console.log(`Grade: ${grade}`);
 document.getElementById("name").innerHTML = "<b>Name: </b><i>" +name+ "</i>";
-document.getElementById("school").innerHTML = "<b>School: </b><i>" +school+ "/i?";
+document.getElementById("school").innerHTML = "<b>School: </b><i>" +school+ "</i>";
 document.getElementById("grade").innerHTML = "<b>Grade: </b><i>" +grade+ "</i>";
