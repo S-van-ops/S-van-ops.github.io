f105699814c3b65dd578f8a30eca9e58501f58e7
S-van-ops <vankayala.tej@gmail.com>
2025-07-10 14:03:21 -0400

    Update jsreview.js

--- jsreview.js
+++ jsreview.js
@@ -1,4 +1,13 @@
 var stemclass = "Technology";
-console.log(`This code was writtten in the STEM ${stemclass}PREP`);
+let myName ="Surya";
+console.log(`This code was writtten in the STEM PREP ${stemclass} class`);
 
-alert("This is an alert!");
+alert(`This code was writtten in the STEM PREP ${stemclass} class by ${myName}`);
+
+var name = prompt("Hello, what is your name?");
+var school = prompt("What school would you attend?");
+var grade = prompt("What grade are you in?");
+
+document.getElementById("name").innerHTML = "Name: <b>" + name + "</b>";
+document.getElementById("school").innerHTML = "School: <b>" + school + "</b>";
+document.getElementById("grade").innerHTML = "Grade: <b>" + grade + "</b>";
