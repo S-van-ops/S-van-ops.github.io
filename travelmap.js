7507585cf57c04f2388c99a1dbec5f3e369b6e18
S-van-ops <vankayala.tej@gmail.com>
2025-07-17 14:28:37 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -14,7 +14,7 @@ let circleMarker01 = L.circle([19.8987,-155.6659], {
   fillColor: "yellow",
   fillOpacity:0.5,
   radius: 500
-  ]).bindPopup("<center>
+}}.bindPopup("<center>
                <h3>Hello from Hawaii!</h3>
               <iframe width="1206" height="315" src="https://www.youtube.com/embed/J_4leOeH9Lc" title="FLYING OVER HAWAII (4K UHD) - Relaxing Music Along With Beautiful Nature Videos(4K Video Ultra HD)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                </center>").addTo(map);
@@ -24,7 +24,7 @@ let volcanoMarker = L.circle([19.4069, -155.2834], {
   fillColor: "red",
   fillOpacity:0.5,
   radius: 500
-  ]).bindPopup("<p>Kīlauea Volcano</p>").addTo(map);
+}}.bindPopup("<p>Kīlauea Volcano</p>").addTo(map);
 let honoluluMarker= L.circle([21.2793,-157.8292], {title: "Honolulu "}).bindPopup("<p>Wakiki Beach</p>").addTo(map);
 let pearlharborMarker = L.circle([21.349270, -157.943970], {title: "Pearl Harbor "}).bindPopup("<p>Pearl Harbor Naval Base </p>").addTo(map);
 
