3faf092bc9c443f861201302f65ee9057dec6703
S-van-ops <vankayala.tej@gmail.com>
2025-07-17 14:24:55 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -9,8 +9,22 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
+let circleMarker01 = L.circle(center, {
+  color: "black",
+  fillColor: "yellow",
+  fillOpacity:0.5,
+  radius: 500
+  ]).bindPopup("<center>
+               <h3>Hello from Hawaii!</h3>
+              <iframe width="1206" height="315" src="https://www.youtube.com/embed/J_4leOeH9Lc" title="FLYING OVER HAWAII (4K UHD) - Relaxing Music Along With Beautiful Nature Videos(4K Video Ultra HD)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+               </center>").addTo(map);
 
-let volcanoMarker = L.marker([19.4069, -155.2834], {title: "Kīlauea Volcano"}).bindPopup("<p>Kīlauea Volcano</p>").addTo(map);
-let honoluluMarker= L.marker([21.2793,-157.8292], {title: "Honolulu "}).bindPopup("<p>Wakiki Beach</p>").addTo(map);
-let pearlharborMarker = L.marker([21.349270, -157.943970], {title: "Pearl Harbor "}).bindPopup("<p>Pearl Harbor Naval Base </p>").addTo(map);
+let volcanoMarker = L.circle([19.4069, -155.2834], {
+  color: "black",
+  fillColor: "red",
+  fillOpacity:0.5,
+  radius: 500
+  ]).bindPopup("<p>Kīlauea Volcano</p>").addTo(map);
+let honoluluMarker= L.circle([21.2793,-157.8292], {title: "Honolulu "}).bindPopup("<p>Wakiki Beach</p>").addTo(map);
+let pearlharborMarker = L.circle([21.349270, -157.943970], {title: "Pearl Harbor "}).bindPopup("<p>Pearl Harbor Naval Base </p>").addTo(map);
 
