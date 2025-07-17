d611d4bbdda45b6d8c6c482371c2131507eed897
S-van-ops <vankayala.tej@gmail.com>
2025-07-17 13:49:36 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -7,3 +7,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+
+let volcanoMarker = L.marker([19.4069, -155.2834], {title: "Kīlauea Volcano"}).bindPopup("<p>Kīlauea Volcano</p>").addTo(map);
+let honoluluMarker= L.marker([21.2793,-157.8292], {title: "Honolulu "}).bindPopup("<p>Wakiki Beach</p>").addTo(map);
+let pearlharborMarker = L.marker([21.349270, -157.943970], {title: "Pearl Harbor "}).bindPopup("<p>Pearl Harbor Naval Base </p>").addTo(map);
+
